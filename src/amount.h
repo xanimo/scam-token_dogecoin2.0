4cfd7e70cb87baa59f9e6527a7360216dfaf2ee2
DogecoinBoss <84752709+DogecoinBoss@users.noreply.github.com>
2021-06-10 08:33:43 -0800

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
-//Copyright (c) 2021 Dogecoin2 Core Devs
+// Copyright (c) 2021 Dogecoin2 Core Devs
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 //Do not alter these values unless lead creator -
